fb20a052957b55b47375e2706f6b65109a228cd8
Corrie Stroup <Corrie.Stroup@gmail.com>
2022-09-12 16:48:13 +0700

    popup form and refactored functions
    Added a popup form for inputting new books (as opposed to the old static form)
    
    Reworked the functions for creating Book objects and refactored the name so that it's easier to understand what they do.
    
    Comment out explaining each section
    
    Got rid of html-hardcoded dummy text

--- scripts.js
+++ scripts.js
@@ -1,6 +1,4 @@
-// keep books tidy
-
-
+// Library object literal, no need for a constructor
 const Library = {
     inventory: [],
     takeStock: function(){
@@ -10,9 +8,17 @@ const Library = {
     }
     
   }
+
+// populating the inventory with some dummy books 
+addBookToLibrary('The Bible', 'A lot', 'JeSuS', 'read');
+addBookToTable();
+addBookToLibrary('The Hobbit', 345, 'JRR Tolkien','unread');
+addBookToTable();
+addBookToLibrary('The Wheel of Time', 766,'Robert Jordan', 'unread');
+addBookToTable();
   
-// book object constructor  
-function Book(title, pages, author = 'unknown', readStatus = 'unread') {
+// book object constructor, libraries need many books, right?
+function Book(title, pages, author, readStatus) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -20,68 +26,69 @@ function Book(title, pages, author = 'unknown', readStatus = 'unread') {
 };
  
 // books gotta be in the library, right?
-function addBook(title, pages, author) {
-  let book = new Book(title,pages, author);
+function addBookToLibrary(title, pages, author, status) {
+  let book = new Book(title,pages,author, status);
   Library.inventory.push(book);
 }
 
-addBook('Lord of the Example', 987, 'Frenso Ulson');
-
-
-
+// grab the values from the open form
 const title = document.querySelector('#form-title');
 const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
+const readStatus = document.querySelector('#readStatus');
 const button = document.querySelector('.addtolibrary');
 
-const booklist = document.querySelector('.example-booklist');
-const authorlist = document.querySelector('.author-list');
-const pagelist = document.querySelector('.page-list');
-
-function addBookFromLibrary(){
+// takes a book in the inventory and puts it on the table
+function addBookToTable(){
   const table = document.querySelector('.book-info-table');
   const row = table.insertRow(-1);
   const cell1 = row.insertCell(0);
   const cell2 = row.insertCell(1);
   const cell3 = row.insertCell(2);
+  const cell4 = row.insertCell(3);
 
   for (let i = 0; i < Library.inventory.length; i++){
     cell1.innerHTML = Library.inventory[i].title;
     cell2.innerHTML = Library.inventory[i].author;
     cell3.innerHTML = Library.inventory[i].pages;
+    cell4.innerHTML = Library.inventory[i].readStatus;
   }
 };
 
+// waits to hear a click on the add button
 button.addEventListener('click', (event) => {
-  addBook(title.value, pages.value, author.value);
-  addBookFromLibrary();
+  addBookToLibrary(title.value, pages.value, author.value, readStatus.value);
+  addBookToTable();
   // clear the fields
   title.value = ''; 
   author.value = '';
   pages.value = '';
+  readStatus.value = '';
 
   // tracking the Library inventory while debugging
-for(let i = 0; i < Library.inventory.length; i++) {
-  console.log(Library.inventory[i]);
-}
+  for(let i = 0; i < Library.inventory.length; i++) {
+    console.log(Library.inventory[i]);
+  }
+  closeForm();
 })
 
+// WIP to remove a book from the table and/or inventory
+function removeBook(title){
+  for (let i = 0; i < Library.inventory; i++){
+    if (Library.inventory[i].title === title){
+      Library.inventory.splice(i, 1);
+    }
+  }
+}
 
-//addBookFromLibrary();
+//removeBook('The Hobbit');
+console.log(Library.inventory)
 
-/*button.addEventListener('click', (event) => {
-  const table = document.querySelector('.book-info-table');
-  const row = table.insertRow(-1);
-  const cell1 = row.insertCell(0);
-  const cell2 = row.insertCell(1);
-  const cell3 = row.insertCell(2);
+// for manipulating the input form
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
 
-  cell1.innerHTML = title.value;
-  cell2.innerHTML = author.value;
-  cell3.innerHTML = pages.value;
-})*/
-
-// tracking the Library inventory while debugging
-for(let i = 0; i < Library.inventory.length; i++) {
-  console.log(Library.inventory[i]);
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
 }
