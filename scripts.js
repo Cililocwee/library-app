284b400441864c67a9237e37b7a3e9195699bf5e
Corrie Stroup <Corrie.Stroup@gmail.com>
2022-09-13 23:06:43 +0700

    status functionality and radio selection
    [JS] Added statusRead functionality so books added with the form keep their radio selected status and that status is displayed in the table
    
    [JS] After adding the radio buttons, I had to figure out a way to retrieve the value from them and store it along with the other values in the Library.inventory
    
    [JS] Removed old unused functions
    [CSS] Styled the radio button group to match with the other input sections
    
    [HTML] Added the radio button group and removed the deprecated Delete Selected button

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-// ATTENTION: See line 68
-
 // Library object literal, no need for a constructor
 const Library = {
   inventory: [],
@@ -14,11 +12,9 @@ const Library = {
         console.log(`Removing ${title}...`);
         Library.inventory.splice(i,1);
         console.log(`${title} removed from Library.inventory`)
-      }
-  
+        }
       }
     }
-
 }
 
 // populating the inventory with some dummy books 
@@ -65,22 +61,53 @@ function addBookToTable() {
   const cell5 = row.insertCell(4);
 
 
-  // REWORK THE HTML ON CELL4 ITS LONG AND I DON'T LIKE IT
-  // MAKE REMOVE BUTTON REMOVE ROW
-  // ^^^^^
-  // ^^^^
-  // ^^^
+  // Constructs the cells dynamically
   for (let i = 0; i < Library.inventory.length; i++) {
     cell1.innerHTML = Library.inventory[i].title;
     cell1.classList.add('book-title-cell');
     cell2.innerHTML = Library.inventory[i].author;
     cell3.innerHTML = Library.inventory[i].pages;
-    //cell4.innerHTML = Library.inventory[i].readStatus;
-    cell4.innerHTML = `<select class="read-status-select"><option value="read">read</option><option value="in-progress">in progress</option><option value="unread">unread</option>`;
+    cell4.innerHTML = Library.inventory[i].readStatus;
+    switch (Library.inventory[i].readStatus){
+      case 'read':
+        cell4.innerHTML = `<select class="read-status-select">
+        <option value="read" selected>read</option>
+        <option value="in-progress">in progress</option>
+        <option value="unread">unread</option>`;
+        break;
+      case 'in progress':
+        cell4.innerHTML = `<select class="read-status-select">
+        <option value="read">read</option>
+        <option value="in-progress" selected>in progress</option>
+        <option value="unread">unread</option>`
+        break;
+      case 'unread':
+        cell4.innerHTML = `<select class="read-status-select">
+        <option value="read">read</option>
+        <option value="in-progress">in progress</option>
+        <option value="unread" selected>unread</option>`
+        break;
+      default:
+        cell4.innerHTML = `<select class="read-status-select">
+                      <option value="read">read</option>
+                      <option value="in-progress">in progress</option>
+                      <option value="unread">unread</option>`
+    }
     cell4.classList.add('book-read-status-cell');
     cell5.innerHTML = '<button class="removal-button">Remove</button>';
-    
   }
+
+  // Adds remove functionality to remove button 
+  // Removes row from the DOM and Book from Library.inventory
+  const removeButtonList = document.querySelectorAll('.removal-button');  
+  removeButtonList.forEach(item => {
+    table.lastChild.lastChild.lastChild.lastChild.onclick = function () {
+      if(confirm("Are you sure?")){
+        Library.removeBook(item.parentNode.parentNode.firstChild.textContent);
+        item.parentNode.parentNode.remove();
+      }
+    };
+  })
 };
 
 // waits to hear a click on the ADD button
@@ -90,7 +117,7 @@ button.addEventListener('click', (event) => {
     closeForm();
     return;
   }
-  addBookToLibrary(title.value, pages.value, author.value, readStatus.value);
+  addBookToLibrary(title.value, pages.value, author.value, displayRadioValue());
   addBookToTable();
   // clear the fields
   title.value = '';
@@ -210,25 +237,9 @@ function sortTable(n) {
   }
 }
 
-/* Deprecated button for deletion, as I'm exploring individual delete buttons
-this is obsolete. It provided a lot of insights into selection and deletion though
-so I will leave it until the final build to make reference to it */
-const deleteButton = document.querySelector('.delete-rows');
-deleteButton.addEventListener('click', (event) => {
-  console.log(`Starting removal of selected items...`)
-  let deleteBuffer = document.querySelectorAll('.selected');
-  deleteBuffer.forEach(item => {
-    // grab the title from row
-    let bufferTitle = item.cells[0].textContent;
-    
-    // log title for debugging
-    console.log(bufferTitle);
-    
-    // remove book from inventory
-    Library.removeBook(bufferTitle);
-
-    //remove book from the DOM
-    item.remove();
-  })
-  console.log('Completed removal of selected items...');
-})
+// get value from radio buttons
+function displayRadioValue() {
+  var radioButtonGroup = document.getElementsByName('optradio');
+  var checkedRadio = Array.from(radioButtonGroup).find((radio) => radio.checked);
+  return checkedRadio.value;
+}
